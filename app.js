1d6566f04c9006da6691e306f98d42c3a081c096
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-04-24 13:23:12 +0200

    created a siglePlay function to know computer and player choices, compare them and give the result of the round

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
-console.log("Starting")
 
-let computerChoice
 
-const computerPlay = function(){
+let computerSelection;
+
+function computerPlay(){
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     console.log(randomNumber);
     if (randomNumber === 1){
-        computerChoice = "Rock";
-    }
+        computerSelection = "rock";
+    };
     if (randomNumber === 2){
-        computerChoice = "Paper";
-    }
+        computerSelection = "paper";
+    };
     if (randomNumber === 3){
-        computerChoice = "Scissors";
-    }
-    console.log(`Computer choice: ${computerChoice}`);
-}
+        computerSelection = "scissors";
+    };
+    return computerSelection;
+};
 
-computerPlay()
+//console.log(computerPlay());
+
+function playRound (playerSelection){
+    switch(playerSelection.toLowerCase() + computerPlay()){
+        case 'scissorspaper':
+        case 'paperrock':
+        case 'rockscissors':
+            console.log(`YOU WIN! ${playerSelection} beats Computer ${computerSelection}`);
+            break;
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            console.log(`YOU LOSE! ${computerSelection} beats your choice: ${playerSelection}`);
+            break;
+        case 'scissorsscissors':
+        case 'paperpaper':
+        case 'rockrock':
+            console.log(`IT'S A DRAW! ${playerSelection} = Computer ${computerSelection}`);
+            break;
+        default:
+            console.log("Your choice is invalid!, the only possible choices are: Rock, Paper or Scissors")
+    };
+};
+playRound()
+
+
