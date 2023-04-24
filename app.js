fd215d1b438403f1043b0fb2cd9749fa83f8782f
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-04-24 18:12:14 +0200

    completed the game function in a cicle of 5 rounds of playRound function and gives you the final score

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 
 
-let computerSelection;
 
 function computerPlay(){
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNumber);
     if (randomNumber === 1){
         computerSelection = "rock";
     };
@@ -17,29 +15,48 @@ function computerPlay(){
     return computerSelection;
 };
 
-//console.log(computerPlay());
-
-function playRound (playerSelection){
-    switch(playerSelection.toLowerCase() + computerPlay()){
+const playRound = function(playerSelection, computerSelection){
+    computerSelection = computerPlay();
+    playerSelection = prompt("Rock, Paper, Scissors, shoot!");
+    switch(playerSelection.toLowerCase() + computerSelection){
         case 'scissorspaper':
         case 'paperrock':
         case 'rockscissors':
-            console.log(`YOU WIN! ${playerSelection} beats Computer ${computerSelection}`);
-            break;
+            return roundResult = `YOU WIN! ${playerSelection} beats Computer ${computerSelection}`;
         case 'paperscissors':
         case 'scissorsrock':
         case 'rockpaper':
-            console.log(`YOU LOSE! ${computerSelection} beats your choice: ${playerSelection}`);
-            break;
+            return roundResult = `YOU LOSE! ${computerSelection} beats your choice: ${playerSelection}`;
         case 'scissorsscissors':
         case 'paperpaper':
         case 'rockrock':
-            console.log(`IT'S A DRAW! ${playerSelection} = Computer ${computerSelection}`);
-            break;
+            return roundResult = `IT'S A DRAW! ${playerSelection} = Computer ${computerSelection}`;
         default:
-            console.log("Your choice is invalid!, the only possible choices are: Rock, Paper or Scissors")
+            return roundResult = alert("Your choice is invalid!, the only possible choices are: Rock, Paper or Scissors")
     };
 };
-playRound()
 
-
+function game(){
+    score = {
+        computer: 0,
+        player: 0
+    };
+    for(i=0; i<5; i++){
+        const roundResult = playRound(i);
+        console.log(roundResult);
+        if(roundResult.includes("WIN")){
+            score.player++;
+        } else if (roundResult.includes("LOSE")){
+            score.computer++;
+        }
+    }
+    if(score.computer > score.player){
+        alert("YOU LOSE!, better luck next time!")
+    } else if (score.computer > score.player){
+        alert("CONGRATULATIONS! YOU ARE THE WINNER!!!")
+    } else if (score.computer = score.player){
+        alert("IT'S A DRAW!")
+    }
+    console.log(score)
+}
+console.log(game())
