975e45f066273268789153013ab8b2215ced0829
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-04-26 20:55:56 +0200

    Game finished, all the odds are handled, if you want to exit game the site will stop till you refresh tu restart the game

--- app.js
+++ app.js
@@ -17,31 +17,30 @@ const playRound = function(playerSelection, computerSelection){
     playerSelection = prompt(`Rock, Paper, Scissors, shoot!`);
     computerSelection = computerPlay();
     if(playerSelection === null){
-        confirm("You hit cancel, are you sure you want to exit the game?")? startRestart() : playRound();
+       !confirm("You hit cancel, do you want to restart the game?") ?  (alert("Good Bye!!!")) : startRestart();
     }
     switch(playerSelection.toLowerCase() + computerSelection){
         case 'scissorspaper':
         case 'paperrock':
         case 'rockscissors':
-            return roundResult = `YOU WIN! ${playerSelection} beats Computer ${computerSelection}`;
+            return roundResult = `YOU WIN! ${playerSelection} beats ${computerSelection}`;
         case 'paperscissors':
         case 'scissorsrock':
         case 'rockpaper':
-            return roundResult = `YOU LOSE! ${computerSelection} beats your choice: ${playerSelection}`;
+            return roundResult = `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
         case 'scissorsscissors':
         case 'paperpaper': 
         case 'rockrock':
-            return roundResult = `IT'S A DRAW!! ${playerSelection} = Computer ${computerSelection}`;
+            return roundResult = `IT'S A DRAW!! ${playerSelection} = ${computerSelection}`;
         default:
             return roundResult = alert("Your choice is invalid!, the only possible choices are: Rock, Paper or Scissors"), playRound();
     };
 };
 
-function startRestart() {
-    confirm("Welcome! You are about to start Rock, Paper, Scissors Game");
-    return game();
+function startRestart(){
+    !confirm("Welcome! You are about to start Rock, Paper, Scissors Game") ? new Error(alert("Good Bye!!!")) : game();
 }
-startRestart()
+startRestart();
 
 function game(){
     score = {
@@ -49,28 +48,25 @@ function game(){
         player: 0
     };
     
-    
     for(i=0; i<5; i++){
         const roundResult = playRound(i);
-        console.log(roundResult);
-        alert(roundResult)
+        alert(roundResult);
         if(roundResult.includes("WIN")){
             score.player++;
         } else if (roundResult.includes("LOSE")){
             score.computer++;
         }
     }
-    let = finalResult = ` Final Score: 
-    PLAYER: ${score.player} 
-    COMPUTER: ${score.computer} `
+
+    let finalResult = ` \nFinal Score: \nPLAYER: ${score.player} \nCOMPUTER: ${score.computer} `;
 
     if(score.computer > score.player){
-        alert(`YOU LOSE!, better luck next time! ${finalResult}`)
+        alert(`YOU LOSE!, better luck next time! ${finalResult}`);
     } else if (score.player > score.computer){
-        alert(`CONGRATULATIONS! YOU ARE THE WINNER!!!${finalResult}`)
+        alert(`CONGRATULATIONS! YOU ARE THE WINNER!!! ${finalResult}`);
     } else if (score.computer == score.player){
-        alert(`IT'S A DRAW! ${finalResult}`)
+        alert(`IT'S A DRAW! ${finalResult}`);
     }
-    startRestart()
+    startRestart();
 }
 
