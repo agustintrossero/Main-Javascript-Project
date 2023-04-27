4f0117a862434ff5aa45d6e919ff60473d07d1b6
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-04-27 13:03:27 +0200

    now you can ssee the score live on each round

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const playRound = function(playerSelection, computerSelection){
     playerSelection = prompt(`Rock, Paper, Scissors, shoot!`);
     computerSelection = computerPlay();
     if(playerSelection === null){
-       !confirm("You hit cancel, do you want to restart the game?") ?  (alert("Good Bye!!!")) : startRestart();
+       confirm("You hit cancel, are you shure you want to exit the game?") ?  (alert("Good Bye!!! \n(Press F5 to start game)")) : playRound();
     }
     switch(playerSelection.toLowerCase() + computerSelection){
         case 'scissorspaper':
@@ -38,7 +38,7 @@ const playRound = function(playerSelection, computerSelection){
 };
 
 function startRestart(){
-    !confirm("Welcome! You are about to start Rock, Paper, Scissors Game") ? new Error(alert("Good Bye!!!")) : game();
+    !confirm("Welcome! You are about to play Rock, Paper, Scissors. \nThe game will be the best five rounds! \nGood Luck!") ? new Error(alert("Good Bye!!! \n(Press F5 to start game)")) : game();
 }
 startRestart();
 
@@ -47,17 +47,21 @@ function game(){
         computer: 0,
         player: 0
     };
+
     
     for(i=0; i<5; i++){
         const roundResult = playRound(i);
-        alert(roundResult);
         if(roundResult.includes("WIN")){
             score.player++;
+            alert(`${roundResult} \nScore\nPlayer: ${score.player} \nComputer: ${score.computer}`);
         } else if (roundResult.includes("LOSE")){
             score.computer++;
+            alert(`${roundResult} \nScore\nPlayer: ${score.player} \nComputer: ${score.computer}`);
+        } else {
+            alert(`${roundResult} \nScore\nPlayer: ${score.player} \nComputer: ${score.computer}`);
         }
     }
-
+    
     let finalResult = ` \nFinal Score: \nPLAYER: ${score.player} \nCOMPUTER: ${score.computer} `;
 
     if(score.computer > score.player){
