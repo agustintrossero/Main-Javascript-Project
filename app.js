0e73a016a8cb900e11bde183a46c9fe012374054
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-10 18:48:17 +0200

    started the UI for the game now it works for 5 rounds by clicking on the player selection, next step will be improve on the user experience to start the game, and for the endgame / reset game

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 
-function computerPlay(){
+const buttons = document.querySelectorAll('button');
+const playerScore = document.getElementById('player-score');
+const tieScore = document.getElementById('tie-score');
+const computerScore = document.getElementById('computer-score');
+const roundDescription = document.getElementById('round-description');
+const roundCount = document.getElementById('round-count');
+
+score = {
+    computer: 0,
+    player: 0,
+    tie: 0
+};
+
+let round = 0
+
+const computerPlay = () =>{
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1){
         computerSelection = "rock";
@@ -13,68 +28,62 @@ function computerPlay(){
     return computerSelection;
 };
 
-const playRound = function(playerSelection, computerSelection){
+const playRound = (playerSelection, computerSelection) =>{
+        switch(playerSelection + computerSelection){
+          case 'scissorspaper':
+          case 'paperrock':
+          case 'rockscissors':
+            score.player++
+              return `YOU WIN! ${playerSelection} beats ${computerSelection}\n`
+          case 'paperscissors':
+          case 'scissorsrock':
+          case 'rockpaper':
+            score.computer++
+              return `YOU LOSE! ${computerSelection} beats ${playerSelection}\n`;
 
-    playerSelection = prompt(`Rock, Paper, Scissors, shoot!`);
-    computerSelection = computerPlay();
-
-    if(playerSelection === null){
-       new Error(alert("Good Bye, see you next time!!! \n(Press F5 to start game)"))
-  }
-    switch(playerSelection.toLowerCase() + computerSelection){
-        case 'scissorspaper':
-        case 'paperrock':
-        case 'rockscissors':
-            return roundResult = `YOU WIN! ${playerSelection} beats ${computerSelection}\n`;
-        case 'paperscissors':
-        case 'scissorsrock':
-        case 'rockpaper':
-            return roundResult = `YOU LOSE! ${computerSelection} beats ${playerSelection}\n`;
-        case 'scissorsscissors':
-        case 'paperpaper': 
-        case 'rockrock':
-            return roundResult = `IT'S A TIE!! ${playerSelection} = ${computerSelection}\n`;
-        default:
-            return roundResult = alert("Your choice is invalid!, the only possible choices are: Rock, Paper or Scissors"), playRound();
-    };
-};
-
-function startRestart(){
-    !confirm("Welcome! You are about to play Rock, Paper, Scissors. \nThe game will be the best five rounds! \nGood Luck!") ? new Error(alert("Good Bye!!! \n(Press F5 to start game)")) : game();
-}
-startRestart();
-
-function game(){
-    score = {
-        computer: 0,
-        player: 0,
-        tie: 0
-    };
-    
-    for(i=0; i<5; i++){
-        const roundResult = playRound(i);
-        if(roundResult.includes("WIN")){
-            score.player++;
-            alert(`Round: ${i+1} \n${roundResult} \nSCORE\nPlayer: ${score.player} \nComputer: ${score.computer} \nTie: ${score.tie}`);
-        } else if (roundResult.includes("LOSE")){
-            score.computer++;
-            alert(`Round: ${i+1} \n${roundResult} \nSCORE\nPlayer: ${score.player} \nComputer: ${score.computer} \nTie: ${score.tie}`);
-        } else {
-            score.tie++;
-            alert(`Round: ${i+1} \n${roundResult} \nSCORE\nPlayer: ${score.player} \nComputer: ${score.computer} \nTie: ${score.tie}`);
-        }
+          case 'scissorsscissors':
+          case 'paperpaper': 
+          case 'rockrock':
+            score.tie++
+             return `IT'S A TIE!! ${playerSelection} = ${computerSelection}\n`;
+          default:
+            return null
+      }; 
     }
-    
-    let finalResult = ` \n\nFINAL SCORE: \nPLAYER: ${score.player} \nCOMPUTER: ${score.computer} \nTIES: ${score.tie} \n\nLet´s go for a Rematch!!!`;
 
-    if(score.computer > score.player){
-        alert(`YOU LOSE!, better luck next time! ${finalResult}`);
-    } else if (score.player > score.computer){
-        alert(`CONGRATULATIONS! YOU ARE THE WINNER!!! ${finalResult}`);
-    } else if (score.computer == score.player){
-        alert(`IT'S A TIE! ${finalResult}`);
-    }
-    startRestart();
+const updateScoreboard = () => {
+    playerScore.textContent = score.player
+    computerScore.textContent = score.computer
+    tieScore.textContent = score.tie
 }
 
+const handleButtonClick = (e) => {
+    const playerSelection = e.target.value;
+    const computerSelection = computerPlay();
+    let roundResult = playRound(playerSelection, computerSelection);
+    round++;
+    roundCount.textContent = round
+    roundDescription.textContent = roundResult
+    if (round === 5) {
+        endGame();
+        return;
+      }
+      updateScoreboard();
+    };
+
+const game = () => {
+        buttons.forEach(button =>{
+        button.addEventListener('click', handleButtonClick)
+    })       
+}
+
+const endGame = () => {
+    buttons.forEach(button => {
+      button.removeEventListener('click', handleButtonClick);
+      button.disabled = true;
+    });
+    updateScoreboard();
+  };
+
+  game()
 
